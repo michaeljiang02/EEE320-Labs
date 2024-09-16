5d8c010650f701b3eccd7b5cd88314187b4402bd
Michael Jiang <michael.jiang02@gmail.com>
2024-09-16 13:11:58 -0400

    Touch Table test passed

--- Lab_3/controller.py
+++ Lab_3/controller.py
@@ -36,10 +36,22 @@ class RestaurantController(Controller):
     def create_ui(self):
         self.view.create_restaurant_ui()
 
+    def table_touched(self, table_index):
+        table = self.restaurant.tables[table_index]
+        table_controller = TableController(self.view, self.restaurant, table)
+        self.view.set_controller(table_controller)
 
 class TableController(Controller):
-    pass
 
+    def __init__(self, view, restaurant, table):
+        super().__init__(view, restaurant)
+        self.table = table
+
+    def create_ui(self):
+        self.view.create_table_ui(self.table)
+
+    def seat_touched(self, seat_number):
+        pass
 
 class OrderController(Controller):
     pass
