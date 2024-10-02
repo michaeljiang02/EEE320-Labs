1aaa9d53c96597f21f7f3d3c9e8ea1c62c5760d1
Michael Jiang <michael.jiang02@gmail.com>
2024-10-02 19:15:10 -0400

    Added some comments to functions, lab ready to submit

--- Lab_3/model.py
+++ Lab_3/model.py
@@ -4,8 +4,10 @@ from constants import TABLES, MENU_ITEMS
 class Restaurant:
 
     def __init__(self):
+        """
+        Restaurant is always initialized with tables and menu items.
+        """
         self.tables = [Table(seats, loc) for seats, loc in TABLES]
-        # TODO: uncomment next line
         self.menu_items = [MenuItem(name, price) for name, price in MENU_ITEMS]
 
 class Table:
@@ -13,13 +15,18 @@ class Table:
     def __init__(self, seats, location):
         self.n_seats = seats
         self.location = location
-        # TODO: Uncomment next line
         self.orders = [Order() for _ in range(seats)]
 
     def order_for(self, seat_number):
+        """
+        Retrieves an order object by indexing into the seat number of a table object.
+        """
         return self.orders[seat_number]
 
     def has_order_for(self, seat_number):
+        """
+        Checks whether the order object at the seat index has items
+        """
         return self.orders[seat_number].items
 
 class Order:
@@ -34,6 +41,9 @@ class Order:
         return [item for item in self.items if item.ordered == False]
 
     def place_new_orders(self):
+        """
+        Marks every item in the order as ordered.
+        """
         for item in self.unordered_items():
             item.mark_as_ordered()
 
@@ -42,6 +52,9 @@ class Order:
             self.items.remove(item)
 
     def total_cost(self):
+        """
+        Returns the sum of every outstanding item in our order.
+        """
         sum = 0
         for item in self.items:
             sum += item.details.price
