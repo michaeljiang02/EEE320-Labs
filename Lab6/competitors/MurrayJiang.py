bf3e042d7343fd3795455c8ba2b5068adeb227d3
murrayasamuel1 <s30711@rmc.ca>
2024-11-27 15:30:06 -0500

    Add files via upload

--- Lab6/competitors/MurrayJiang.py
+++ Lab6/competitors/MurrayJiang.py
@@ -1,30 +1,31 @@
 """
-An aggressive growth and momentum based bug. Uses three phases.
-Phase one - move away from parent and eat until an energy threshold is reached. Then move to phase 2
+A medium momentum and poison based bug. Uses three phases.
+Phase one - move away from parent and eat until an energy threshold nmor turn threshold is reached. Then move to phase 2
 Phase two - grow photo-glands and propagate until an enemy is seen. Then move to phase 3
-Phase three - stop propagating to conserve energy, attack enemy if it has less strength, otherwise run.
+Phase three - stop propagating to conserve energy, attack enemy if it has less strength, otherwise create poison and stay still
 
 version 1
 2024-11-27
 """
 
-from shared import Creature, Cilia, CreatureTypeSensor, EnergySensor, Propagator, Direction, Soil, PhotoGland, Plant
+from shared import Creature, Cilia, CreatureTypeSensor, EnergySensor, Propagator, Direction, Soil, PhotoGland, Plant, PoisonGland
 
 class KoublaiKhan(Creature):
     __instance_count = 0
 
     def __init__(self):
         super().__init__()
-        KoublaiKhan.__instance_count += 1
+        KoublaiKhan.__instance_count+=1
         self.cilia = None
         self.type_sensor = None
         self.energy_sensor= None
         self.womb = None
-        self.branch= []
+        self.poison_gland=None
         self.phase=1
-        self.has_moved=0
         self.spread_direction=Direction.random()
         self.conserve_count=0
+        self.early_game_count=0
+        self.photo_gland_count=0
 
     @classmethod
     def instance_count(cls):
@@ -42,44 +43,43 @@ class KoublaiKhan(Creature):
         if self.phase==3:
             self.phase_3()
 
-    def phase_1(self): #rapid expansion based on quickly eating plants
+    def phase_1(self): #rapid expansion based on quick movement and growing photo glands
+        self.early_game_count+=1
         if not self.cilia and not self.type_sensor:
             self.grow_initial_organs()
         if self.cilia and self.type_sensor:
             self.eat_plants()
-        if self.strength()>1550:
-            self.womb=KoublaiKhanPropagator(self)
+        if  self.early_game_count>5:
             self.phase=2
 
-    def phase_2(self): #propagate by growing photo glands
-        if self.energy_sensor:
-            self.energy_sensor=None
-        while (self.strength()>(PhotoGland.CREATION_COST) and len(self.branch)<7):
-            self.branch.append(PhotoGland(self))
+    def phase_2(self): #stay still and propagate
+        if not self.womb and self.strength() > Propagator.CREATION_COST+100:
+            self.womb = KoublaiKhanPropagator(self)
         self.reproduce_if_able()
         if self.look_for_enemies(): #if an enemy is spotted, move to phase 3.
             self.conserve_count=0
             self.phase=3
 
-    def phase_3(self): #avoid reproducing to save energy. If an enemy has more energy, run. Else, attack.
+    def phase_3(self): #stay still and avoid reproducing to save energy. If an enemy has less energy, attack. Else, make poison.
         self.conserve_count+=1
         if not self.energy_sensor:
-            self.branch.pop(-1) #remove a photo gland to make room for an energy sensor
             self.energy_sensor=EnergySensor(self)
         if (self.maneuver_warfare()):
-            self.conserve_count += 0
-        if self.conserve_count>=5:
+            self.conserve_count = 0
+        if self.conserve_count>=4:
             self.phase = 2
 
     def reproduce_if_able(self):
-        while self.strength() >= 0.33 * Creature.MAX_STRENGTH:
-            self.womb.give_birth(self.strength()/3, self.spread_direction)
+        if self.strength() >= 0.95 * Creature.MAX_STRENGTH:
+            self.womb.give_birth(self.strength()/2, self.spread_direction)
 
     def grow_initial_organs(self):
-        if not self.type_sensor and self.strength()>CreatureTypeSensor.CREATION_COST:
-            self.type_sensor = CreatureTypeSensor(self)
-        if not self.cilia and self.strength() > Cilia.CREATION_COST:
+        if not self.cilia and self.strength() > Cilia.CREATION_COST+100:
             self.cilia = Cilia(self)
+        if not self.type_sensor and self.strength() > CreatureTypeSensor.CREATION_COST+100:
+            self.type_sensor = CreatureTypeSensor(self)
+        while (self.photo_gland_count < 5) and self.strength() > PhotoGland.CREATION_COST+100:
+            PhotoGland(self)
 
     def look_for_enemies(self):
         for d in Direction:
@@ -96,7 +96,9 @@ class KoublaiKhan(Creature):
                 if threat_strength<self.strength():
                     self.cilia.move_in_direction(d)
                 else:
-                    self.cilia.move_in_direction(d.opposite())
+                    if self.strength() > PoisonGland.CREATION_COST:
+                        self.poison_gland=PoisonGland(self)
+                        self.poison_gland.add_poison(self.strength()*0.8)
             return True
         return False
 
